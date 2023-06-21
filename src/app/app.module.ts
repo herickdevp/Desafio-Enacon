3451324b233599083adf215f486cbf42ba70726a
Herick Silva <herick610@gmail.com>
2023-06-21 12:08:14 -0300

    Criação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CtaComponent } from './components/cta/cta.component';
+import { IntroducaoComponent } from './components/home/introducao/introducao.component';
+import { ProjetosComponent } from './components/home/projetos/projetos.component';
+import { FormacaoComponent } from './components/home/formacao/formacao.component';
+import { ExperienciaComponent } from './components/paginas/experiencia/experiencia.component';
+import { ContatoComponent } from './components/paginas/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CtaComponent,
+    IntroducaoComponent,
+    ProjetosComponent,
+    FormacaoComponent,
+    ExperienciaComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
