aed08c6b956c9f4eea40e7bc8780499d09b152e4
Herick Silva <herick610@gmail.com>
2023-06-21 13:29:28 -0300

    criacao do componente header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { HomeComponent } from './components/home/home/home.component';
 import { ExperienciaComponent } from './components/paginas/experiencia/experiencia.component';
 import { ContatoComponent } from './components/paginas/contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'experiencia', component: ExperienciaComponent },
+  { path: 'contato', component: ContatoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
